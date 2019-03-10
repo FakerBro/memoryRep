e3366896b1fc494cb1ec2c82e7dbdb98c3c9c103
FakerBro <pakmur1991@o2.pl>
2019-03-10 11:23:50 +0100

    Update memtest.js

--- memtest.js
+++ memtest.js
@@ -1,3 +1,22 @@
-var msg = "hello world";
-console.log(msg);
+const memoryGame = {
+    tileCount : 20, //liczba klocków
+    tileOnRow : 5, //liczba klocków na rząd
+    divBoard : null, //div z planszą gry
+    divScore : null, //div z wynikiem gry
+    tiles : [], //tutaj trafi wymieszana tablica klocków
+    tilesChecked : [], //zaznaczone klocki
+    moveCount : 0, //liczba ruchów
+    tilesImg : [ //grafiki dla klocków
+        'title_1.png',
+        'title_2.png',
+        'title_3.png',
+        'title_4.png',
+        'title_5.png',
+        'title_6.png',
+        'title_7.png',
+        'title_8.png',
+        'title_9.png',
+        'title_10.png'
+    ]
+}
 
